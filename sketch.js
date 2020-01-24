e0ac58ce330c4b6f14fc34317e8d4692bda78d2d
chiyo <ttl2132@columbia.edu>
2020-01-24 06:52:59 -0500

    fixed spacing

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-	createCanvas(800,600); // make an HTML canvas element width x height pixels
+	createCanvas(800, 600); // make an HTML canvas element width x height pixels
 	frameRate(1);
 }
 
@@ -19,15 +19,16 @@ function draw() {
 
 	c = color(173, 97, 255, 250);
 	fill(c);
-	triangle(100, 100, 400, 525, 100 + 600/60*minute(), 100);
+	triangle(100, 100, 400, 525, 100 + 600/60 * minute(), 100);
 
     c = color(82, 82, 255, 200);
 	fill(c);
-	triangle(400 - 300/12 * hour(), 525 - 425/12*hour(), 400, 525, 700, 100);
+	triangle(400 - 300/12 * hour(), 525 - 425/12 * hour(), 400, 525, 700, 100);
 
 	c = color(255, 204, 107, 150);
 	fill(c);
-	triangle(100, 100, 700 - 300/60 * second() , 100 + 425/60 * second() , 700, 100)
+	triangle(100, 100, 700 - 300/60 * second(), 100 + 425/60 * second(), 700, 100)
+
 	if (second() == 0) {
         console.log(minute());
     }
