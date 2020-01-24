d82f69f762de454eeda7388b6189925e2154a2fb
chiyo <ttl2132@columbia.edu>
2020-01-24 07:16:59 -0500

    used the mapping function

--- sketch.js
+++ sketch.js
@@ -12,15 +12,15 @@ function draw() {
 
 	c = color(173, 97, 255, 250);
 	fill(c);
-	triangle(100, 100, 400, 525, 100 + 600/60 * minute(), 100);
+	triangle(100, 100, 400, 525, map(minute(), 0, 60, 100, 700), 100);
 
     c = color(82, 82, 255, 200);
 	fill(c);
-	triangle(400 - 300/12 * hour(), 525 - 425/12 * hour(), 400, 525, 700, 100);
+	triangle(map(hour(), 0, 12, 400, 100), map(hour(), 0, 12, 525, 100), 400, 525, 700, 100);
 
 	c = color(255, 204, 107, 150);
 	fill(c);
-	triangle(100, 100, 700 - 300/60 * second(), 100 + 425/60 * second(), 700, 100)
+	triangle(100, 100, map(second(), 0, 60, 700, 400), map(second(), 0, 60, 100, 525), 700, 100);
 
 	if (second() == 0) {
         console.log(minute());
