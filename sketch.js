1dc9c1854b6622a543e357a7afc72f6a6c22a099
chiyo <ttl2132@columbia.edu>
2020-01-24 06:31:29 -0500

    Created clock

--- sketch.js
+++ sketch.js
@@ -1,9 +1,15 @@
 function setup() {
 	createCanvas(800,600); // make an HTML canvas element width x height pixels
+	frameRate(1);
+
 }
 
 function draw() {
 	background(225);
+	c = color(255);
+	fill(c);
+	noStroke();
+	triangle(100, 100, 400, 525, 700, 100);
 	textSize(32);
 	fill(180);
 	text(hour(), 10, 30);
@@ -11,4 +17,17 @@ function draw() {
 	text(minute(), 10, 60);
 	fill(0);
 	text(second(), 10, 90);
+
+	c = color(173, 97, 255, 250);
+	fill(c);
+	triangle(100, 100, 400, 525, 100 + 600/60*minute(), 100);
+
+    c = color(82, 82, 255, 200);
+	fill(c);
+	triangle(400 - 300/12 * hour(), 525 - 425/12*hour(), 400, 525, 700, 100);
+
+	c = color(255, 204, 107, 150);
+	fill(c);
+	triangle(100, 100, 700 - 300/60 * second() , 100 + 425/60 * second() , 700, 100)
+
 }
